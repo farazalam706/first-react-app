35a0e54cf68f7d47019cc94ea931a5f855ea8fd6
githubbiee <github9988@gmail.com>
2022-02-04 19:03:04 +0500

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <p>
+            Header Component
+        </p>
+    )
+}
+
+export function Footer() {
+    return (
+        <p>
+            Header Component
+        </p>
+    )
+}
